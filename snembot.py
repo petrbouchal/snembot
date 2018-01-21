f557306ac384be78f0460a00442c740cbe9ef06d
petrbouchal <pbouchal@gmail.com>
2018-01-21 12:40:29 +0100

    first

---
+++ snembot.py
@@ -0,0 +1,25 @@
+import tweepy, time, sys, os, csv, numpy as np, pandas as pd, random as rd
+
+# this part only to set and retrieve secrets in another file - omit when deployed
+
+import set_secrets
+set_secrets.set_secrets_env()
+print(os.environ["TWITTER_SNEMBOT_CKEY"])
+
+# load data
+txt = pd.read_csv("./texts.csv")
+
+print(txt)
+
+# select piece to tweet
+
+twt = txt["text"][rd.randrange(0, len(txt["text"]))]
+print(twt)
+
+# authenticate
+auth = tweepy.OAuthHandler(os.environ["TWITTER_SNEMBOT_CKEY"], os.environ["TWITTER_SNEMBOT_CSECRET"])
+auth.set_access_token(os.environ["TWITTER_SNEMBOT_AKEY"], os.environ["TWITTER_SNEMBOT_ASECRET"])
+api = tweepy.API(auth)
+
+# api.update_status(twt)
+
